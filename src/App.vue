7ab29327dc6eca0b5d55201ad336b6f7d830c427
Tim Chen <29265905+timyc@users.noreply.github.com>
2022-10-09 21:23:56 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="p-relative">
+    <nav class="navBar fixed">
+      <div class="navBar-container">
+        <ul class="navBar-list">
+          <li class="navBar-item">
+            <RouterLink to="/" class="navBar-link">Home</RouterLink>
+          </li>
+          <li class="navBar-item">
+            <RouterLink to="/" class="navBar-link">About</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+
+  <RouterView />
+</template>
+
+<style scoped>
+.navBar-link {
+  color: white !important;
+  text-decoration: none;
+}
+
+.active {
+  color: #42b983 !important;
+}
+
+.navBar-item {
+  font-size: 16px;
+  padding: .75em 15px;
+  letter-spacing: 1px;
+  position: relative;
+  font-weight: 700;
+}
+
+.navBar-list {
+  display: flex;
+  flex-direction: row;
+  padding-left: 0;
+  margin-bottom: 0;
+  list-style: none;
+}
+
+.navBar-container {
+  display: flex !important;
+  flex-basis: auto;
+  flex-grow: 1;
+  align-items: center;
+  justify-content: right;
+}
+
+.navBar {
+  background-color: transparent;
+  height: 94px;
+  display: flex !important;
+  transition: all .6s ease;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="headSplash p-relative">
+    <div class="particles">
+      <img src="@/assets/particles_blurred.png" alt="spinning particles" class="animation-image" />
+    </div>
+    <div class="container">
+      <el-row>
+        <el-col>
+          <div class="wrapper p-relative">
+            <div class="content">
+              <h1>Certitude is Seamless</h1>
+              <p>
+                Our revolutionary education verification
+                platform is simple, instant, and secure
+              </p>
+              <div class="buttons">
+                <el-button class="bgradient" type="warning" round>I am a recruiter</el-button>
+                <el-button class="bgradient" round>I am a Student</el-button>
+              </div>
+            </div>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.container {
+  max-width:960px;
+  width: 100%;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+.headSplash {
+  background-image: url('@/assets/splashbanner.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: 50%;
+  overflow: hidden;
+}
+
+.particles {
+  position: absolute;
+  left: 10%;
+}
+
+.animation-image {
+  width: 30vw;
+  animation: spin 20s linear infinite;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+.content {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 40%;
+}
+</style>
