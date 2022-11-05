afb019f0f7d340272987472652294ac11d633635
Jeff Hover <hoverj@uci.edu>
2022-11-04 20:08:07 -0700

    I don't even know. Trying to make the student info page but im dumb

---
+++ src/views/StudentInfo.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="student">
+      <h1>This is a information about students page</h1>
+    </div>
+    <div class = "container info-display">
+    <p>Login to your univeristy network in app</p>
+    <p>Click to generate a link/QR code</p>
+    <p>Share the link with a participating business</p>
+  </div>
+  
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+
+    .student {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+      background-color: white;
+    }
+    .info-display{
+        color: red;
+        width: 60.47vh;
+        height: 48.71vh;
+        display: flex;
+        align-items: center;
+    }  
+  
+  }
+  </style>
+
