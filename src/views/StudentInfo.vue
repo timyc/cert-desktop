c889178340b96ee0af0e8b4782eaad7e9fa42503
Jeff Hover <hoverj@uci.edu>
2022-11-09 23:10:32 -0800

    Some work on the student display page

--- src/views/StudentInfo.vue
+++ src/views/StudentInfo.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="student" >
     <Ccard class = "info-display">
-      <CCardHeader>Header</CCardHeader>
+      <CCardHeader></CCardHeader>
       <CListGroup flush>
-        <CCardGroupItem><p class = "text-display">Login to your univeristy network in app</p> <img class="image-location" src="../assets/log_in.jpg" alt=""></CCardGroupItem>
-        <CCardGroupItem><p>Click to generate a link/QR code</p></CCardGroupItem>
-        <CCardGroupItem><p>Share the link with a participating business</p></CCardGroupItem>
+        <CCardGroupItem><p class = "text-display item-left">Login to your univeristy network in app</p> <img class="image-location img-right" src="../assets/log_in.jpg" alt=""></CCardGroupItem>
+        <CCardGroupItem><p class = "text-display item-right" >Click to generate a link/QR code</p><img class="image-location img-left" src="../assets/log_in.jpg" alt=""></CCardGroupItem>
+        <CCardGroupItem><p class = "text-display item-left">Share the link with a participating business</p><img class="image-location img-right" src="../assets/log_in.jpg" alt=""></CCardGroupItem>
       </CListGroup>
     </Ccard>
   </div>
@@ -23,8 +23,8 @@
     }
     .info-display{
         background-color: white;
-        width: 60.47vh;
-        height: 48.71vh;
+        width: 70vh;
+        height: 60vh;
         display: flex;
         align-items: center;
     }
@@ -34,10 +34,31 @@
     }
     .image-location{
       position: relative;
-      width: 10vh;
-      height: 10vh;
-    }  
-  
+      width: 14vh;
+      height: 14vh;
+    }
+
+    .item-left{
+      top: 5vh;
+      font-size: large;
+      font-weight: bolder;
+    }
+    .item-right{
+      left: 30vh;
+      top: 5vh;    
+      font-size: large;
+      font-weight: bolder;
+    }
+    .img-right{
+      left: 50vh;
+      bottom: 2vh;
+    }
+
+    .img-left{
+      bottom: 5vh;
+      left: 2vh;
+    }
+
   }
   </style>
 
