220248dc5120fdca7e35205246300f553e5527a2
Jeff Hover <hoverj@uci.edu>
2022-11-14 20:31:08 -0800

    Finalzing student page. Need to create a new config for vue

--- src/views/StudentInfo.vue
+++ src/views/StudentInfo.vue
@@ -1,34 +1,73 @@
 <template>
-    <div class="student" >
+  <p class = "logo"><span class = "logo1">Cert</span><span class = "logo2">itude</span></p>
+    <div class="student">
     <Ccard class = "info-display">
-      <CCardHeader></CCardHeader>
-      <CListGroup flush>
-        <CCardGroupItem><p class = "text-display item-left">Login to your univeristy network in app</p> <img class="image-location img-right" src="../assets/student_info_pic1.jpg" alt=""></CCardGroupItem>
-        <CCardGroupItem><p class = "text-display item-right" >Click to generate a link/QR code</p><img class="image-location img-left" src="../assets/student_info_pic2.png" alt=""></CCardGroupItem>
-        <CCardGroupItem><p class = "text-display item-left">Share the link with a participating business</p><img class="image-location img-right" src="../assets/student_info_pic3.jpg" alt=""></CCardGroupItem>
+      <CCardHeader class = "header">How it Works</CCardHeader>
+      <CListGroup>
+        <CCardGroupItem><p class = "text-display item-left">Login to your univeristy network in app</p> <img class="image-location img-right" src="@/assets/student_info_pic1.jpg" alt=""></CCardGroupItem>
+        <CCardGroupItem><p class = "text-display item-right" >Click to generate a link/QR code</p><img class="image-location img-left" src="@/assets/student_info_pic2.png" alt=""></CCardGroupItem>
+        <CCardGroupItem><p class = "text-display item-left">Share the link with a participating business</p><img class="image-location img-right" src="@/assets/student_info_pic3.jpg" alt=""></CCardGroupItem>
       </CListGroup>
     </Ccard>
   </div>
-  <footer>
-    
-  </footer>
+  <img class="man-standing" src="@/assets/lawyer_standin.png" alt="">
+    <el-footer class="footer" height="300">
+      <div>
+      <p>educertified@gmail.com</p>
+  <img class = "socials" src="@/assets/linkedin_logo.png" alt="">
+  <img class = "socials" src="@/assets/instagram_logo.png" alt="">
+</div>
+  </el-footer>
   </template>
   
   <style>
   @media (min-width: 1024px) {
+    
+    .socials{
+      width: 5vh;
+      margin-left: auto;
+    }
+
+    .logo{
+      font-weight: bolder;
+      font-size: 2rem;
+      background: darkslategray;;
+      padding: 0%;
+      margin: 0%;
+    }
+
+    .logo1{
+      color: #65CC4C;
+    }
+    .logo2{
+      color: #161616;
+    }
+    .man-standing{
+      position: absolute;
+      right: 40vh;
+      bottom: 10vh;
+      width: 40vh;
+    }
+
+    .header{
+      text-align: center;
+      font-weight: bold;
+      font-size: large;
+      position: relative;
+      left: 25vh;
+    }
 
     .student {
       min-height: 100vh;
       display: flex;
       align-items: center;
       justify-content: center;
-      background-color: #FAFAFA;
+      background-color: #D3D3D3;
     }
     .info-display{
         background-color: white;
         width: 70vh;
         height: 60vh;
-        display: flex;
         align-items: center;
     }
     .text-display{
@@ -54,7 +93,7 @@
     }
     .img-right{
       left: 50vh;
-      bottom: 2vh;
+      bottom: 5vh;
     }
 
     .img-left{
@@ -63,5 +102,13 @@
     }
 
   }
+  .footer {
+  background: darkslategray;
+  padding: 1em;
+  color: white;
+  text-align: center;
+  margin-bottom: 0;
+}
+
   </style>
 
