47bdaf348434b8505d692e343be22bec3454a2fc
Jason Nguyen <nguyenjason8830@gmail.com>
2022-11-28 15:11:30 -0800

    navbar and stylings to main

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,7 +13,7 @@ export default defineComponent({
   components: {FirebaseAuth},
   methods: {
     studentClick() {
-      window.location.replace("https://m.certitude-demo.delta.games");
+      window.location.replace("https://cert-tawny.vercel.app/");
     },
     handleClose(done: () => void) {
       ElMessageBox.confirm('Are you sure to close this dialog?')
@@ -29,39 +29,39 @@ export default defineComponent({
 </script>
 
 <template>
-  <header class="headSplash p-relative" id="home">
-    <div class="particles">
+<header class="headSplash p-relative" id="home">
+    <!-- <div class="particles">
       <img src="@/assets/particles_blurred.png" alt="spinning particles" class="animation-image" />
-    </div>
+    </div> -->
     <div class="container">
       <el-row>
         <el-col>
-          <div class="wrapper p-relative">
-            <div class="content">
-              <h1>Certitude is Seamless</h1>
-              <p>
+          <div class="wrapper p-relative overall-content">
+            <div class="content text-cert">
+              <h1 class="text-wrapper">Certitude is <span class="seam">Seamless</span> </h1>
+              <p class="intro-text">
                 Our revolutionary education verification
-                platform is simple, instant, and secure.
+                platform is <br/>simple, instant, and secure
               </p>
               <div class="buttons">
-                <el-button class="bgradient" type="warning" @click="dialogVisible = true" round>I am a recruiter</el-button>
-                <el-button class="bgradient" @click="studentClick" round>I am a Student</el-button>
+                <!-- <el-button class="bgradient" type="warning" @click="dialogVisible = true" round>I am a recruiter</el-button>
+                
+                <el-button class="bgradient" @click="studentClick" round>I am a Student</el-button> -->
+                
+                <button class="student-btn" @click="studentClick">Student<el-icon><CircleCheckFilled /></el-icon></button>
+                <br/>
+                <button class="recruiter" @click="dialogVisible = true">Recruiter <el-icon><Avatar /></el-icon></button>
+                <br/>
+
+                <!-- <button class="student">I am a Student <el-icon><Notebook /></el-icon></button> -->
               </div>
             </div>
-          </div>
-        </el-col>
-      </el-row>
-    </div>
-  </header>
-  <el-row id="about">
-    <div class="container">
-      <el-row>
-        <el-col :span="12">
-          <div class="wrapper p-relative">
-            <div class="secondary-content">
-              <el-card class="box-card center">
-                <div class="text item">
-                  <el-icon :size="30" color="#ff7315" class="no-inherit">
+            
+            <img class="worker" src="@/assets/workers.png"/>
+              <el-card class=" card-box">
+                <div class="card-horizontal">
+                <div class="text center item">
+                  <el-icon :size="30" color="#fcb531" class="icon-grad">
                     <Pointer />
                   </el-icon>
                   <h2>One-Click Check</h2>
@@ -69,13 +69,9 @@ export default defineComponent({
                     Students send credentials instantly through a secure link
                   </p>
                 </div>
-                <el-divider>
-                  <el-icon>
-                    <star-filled />
-                  </el-icon>
-                </el-divider>
-                <div class="text item">
-                  <el-icon :size="30" color="#ff7315" class="no-inherit">
+                <div class="vertical-bar"></div>
+                <div class="text center item">
+                  <el-icon :size="30" color="#fcb531" class="icon-grad">
                     <SetUp />
                   </el-icon>
                   <h2>Data Belongs to Students</h2>
@@ -83,14 +79,42 @@ export default defineComponent({
                     Business won't share data on signup and only see what you send
                   </p>
                 </div>
+              </div>
               </el-card>
-            </div>
+            
           </div>
         </el-col>
+      </el-row>
+    </div>
+  </header>
+
+  <el-row id="about">
+    <div class="container">
+      <el-row>
         <el-col :span="12" style="padding-left:50px">
           <div class="wrapper p-relative">
-            <div class="secondary-content">
-              <h1>About Us</h1>
+            
+          <div class="wrap">
+            <div class="secondary-content"> 
+              <h1 class="about-us">About Us</h1>
+
+              <el-card class="about-card">
+                <el-container >
+                  <el-aside width="200px"><img class = "coo-img" src="../assets/coo.PNG"/> </el-aside>
+                  <el-main class="coo-spacing subtitle">Rishabh Harish <div class="no-inherit">COO</div></el-main>
+                 
+                  
+                </el-container>
+              </el-card>
+
+              <el-card class="about-card2">
+                <el-container>
+
+                  <el-aside width="200px"><img class = "cto-img" src="../assets/cto.PNG"/> </el-aside>
+                  <el-main class="cto-spacing subtitle">Bobby Missirian <div class="no-inherit">CTO</div></el-main>
+                </el-container>
+              </el-card>
+
               <p>
                 We are a team of students and professionals who are passionate about education and technology. We
                 believe that education is the key to a better future and that technology can help us achieve that
@@ -98,6 +122,8 @@ export default defineComponent({
                 other's credentials in a seamless and secure way.
               </p>
             </div>
+            <img class = "about-img" src="../assets/about.png"/> 
+            </div> 
           </div>
         </el-col>
       </el-row>
@@ -109,13 +135,13 @@ export default defineComponent({
         <el-col :span="24">
           <div class="wrapper p-relative">
             <div class="secondary-content">
-              <h2>Contact Us!</h2>
-              <el-form size="large" label-width="120px" class="demo-dynamic">
-                <el-form-item prop="email" label="Email">
+              <h1 class ="contact-us">Contact Us!</h1>
+              <el-form size="large" label-width="120px" class="demo-dynamic contact-form">
+                <el-form-item prop="email" label="Email" class="email-area">
                   <el-input />
                 </el-form-item>
-                <el-form-item label="Message" prop="desc">
-                  <el-input type="textarea" rows="15" />
+                <el-form-item label="Message" prop="desc" class="message-area">
+                  <el-input type="textarea" rows="8" />
                 </el-form-item>
                 <el-form-item>
                   <el-button type="primary">Submit</el-button>
@@ -128,12 +154,9 @@ export default defineComponent({
     </div>
   </el-row>
   <el-footer class="footer" height="300">
-    I guess copyright or something can go here<br />
-    should also make this<br />
-    taller<br />
-    with links<br />
-    and stuff
+    Certitude © 2022 Made by Tim Chen, Jeffery Hover, Jason Nguyen, Ben Mueller 
   </el-footer>
+
   <el-dialog v-model="dialogVisible" title="Employer Auth" width="30%" :before-close="handleClose">
     <FirebaseAuth />
     <template #footer>
