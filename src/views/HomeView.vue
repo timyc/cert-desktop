47410cb92e7ac506dc906b0de1733a8af74f330d
Jason Nguyen <nguyenjason8830@gmail.com>
2022-11-28 16:20:48 -0800

    About cards plsdssdsd

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -92,53 +92,6 @@ export default defineComponent({
 
 
 
-
-
-
-<!-- 
-  <el-row id="about">
-    <div class="container">
-      <el-row>
-        <el-col :span="12" style="padding-left:50px">
-          <div class="wrapper p-relative">
-            <div class="secondary-content"> 
-
-              <h1 class="about-us">About Us</h1>
-
-              <el-card class="about-card">
-                <el-container >
-                  <el-aside width="200px"><img class = "coo-img" src="../assets/coo.PNG"/> </el-aside>
-                  <el-main class="coo-spacing subtitle">Rishabh Harish <div class="no-inherit">COO</div></el-main>
-                </el-container>
-              </el-card>
-
-              <el-card class="about-card2">
-                <el-container>
-
-                  <el-aside width="200px"><img class = "cto-img" src="../assets/cto.PNG"/> </el-aside>
-                  <el-main class="cto-spacing subtitle">Bobby Missirian <div class="no-inherit">CTO</div></el-main>
-                </el-container>
-              </el-card>
-
-              <p>
-                We are a team of students and professionals who are passionate about education and technology. We
-                believe that education is the key to a better future and that technology can help us achieve that
-                future. We are building a platform that will help students and businesses connect and verify each
-                other's credentials in a seamless and secure way.
-              </p>
-            </div>
-            <img class = "about-img" src="../assets/about.png"/> 
-            </div>
-            
-
-        </el-col>
-      </el-row>
-    </div>
-  </el-row> -->
-
-
-
-
   <el-row id="about">
     <div class="container">
       <el-row>
@@ -150,7 +103,8 @@ export default defineComponent({
 
               <el-card class="about-card">
                 <el-container >
-                  <el-aside><img class = "coo-img" src="../assets/coo.png"/> </el-aside>
+                  <!-- <img class = "coo-img" src="../assets/coo.png"/>  -->
+                  <el-aside></el-aside>
                   <el-main class="coo-spacing subtitle">Rishabh Harish <div class="no-inherit">COO</div></el-main>
                 </el-container>
               </el-card>
